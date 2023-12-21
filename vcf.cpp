5cfa16fff4dd3393b152dcfaf3f269303f2637c6
Ale <alexcoppe@gmail.com>
2023-12-21 12:35:47 +0100

    Finished the vcf class which has 8 fixed mandatory columns

--- vcf.cpp
+++ vcf.cpp
@@ -2,39 +2,64 @@
 #include <fstream>
 #include<getopt.h>
 #include<list>
+#include <array>
 
 
 class Vcf{
     public:
         std::string chromosome;
         int position;
+        std::string id;
+        std::string reference;
+        std::string alternative;
+        float quality;
+        std::string filter;
+        std::string info;
 
         Vcf();
         Vcf(std::string,
-                int);
+                int, std::string, std::string, std::string, float, std::string, std::string);
         void show();
-        void vcf_from_vcf_line(std::string);
+        Vcf vcf_from_vcf_line(std::string);
 };
 
 
 Vcf::Vcf(){
     chromosome = "";
     position = 0;
+    id = "";
+    reference = "";
+    alternative = "";
+    quality = 0;
+    filter = ".";
+    info = ".";
 }
 
-Vcf::Vcf(std::string chr, int pos){
+Vcf::Vcf(std::string chr, int pos, std::string genomic_id, std::string ref, std::string alt,
+        float qual, std::string filt, std::string inf){
     chromosome = chr;
     position = pos;
+    id = genomic_id;
+    reference = ref;
+    alternative = alt;
+    quality = qual;
+    filter = filt;
+    info = inf;
 }
 
 void Vcf::show(){
     std::cout << "Chromosome: " << chromosome << "\n";
     std::cout << "Position: " << position << "\n";
+    std::cout << "Id: " << id << "\n";
+    std::cout << "Ref: " << reference << "\n";
+    std::cout << "Alt: " << alternative << "\n";
+    std::cout << "Quality: " << quality << "\n";
+    std::cout << "Filter: " << filter << "\n";
+    std::cout << "Info: " << info << "\n";
 }
 
 
-void vcf_from_vcf_line(std::string l){
-    //std::cout << "Running\n";
+Vcf vcf_from_vcf_line(std::string l){
     size_t pos = 0;
     std::string token;
     std::list<std::string> fields;
@@ -44,23 +69,36 @@ void vcf_from_vcf_line(std::string l){
 
     while ((pos = l.find("\t")) != std::string::npos){
         token = l.substr(0, pos);
-        //std::cout << token << "\n";
 
         if (i == 0)
             v.chromosome = token;
         if (i == 1)
             v.position = stoi(token);
+        if (i == 2)
+            v.id = token;
+        if (i == 3)
+            v.reference = token;
+        if (i == 4)
+            v.alternative = token;
+        if (i == 5){
+            if (token == ".")
+                v.quality = 0;
+            else
+                v.quality = std::stof(token);
+        }
+        if (i == 6)
+            v.filter = token;
+        if (i == 7)
+            v.info = token;
 
         fields.push_back(token);
         l = l.substr(pos + 1, l.length());
         i++;
     }
 
-    v.show();
-
     token = l.substr(pos + 1, l.length());
-    //std::cout << token << "\n";
     fields.push_back(token);
+    return v;
 }
 
 int main(int argc, char *argv[]){
@@ -104,7 +142,8 @@ int main(int argc, char *argv[]){
         while (getline(file, line)){
             if (line[0] != '#'){
                 if (line[0] != '#'){
-                    vcf_from_vcf_line(line);
+                    v = vcf_from_vcf_line(line);
+                    v.show();
                     //std::cout << line << "\n";
                 }
             }
@@ -112,8 +151,6 @@ int main(int argc, char *argv[]){
         file.close();
     }
 
-    //Vcf v("fdf", 22);
-    //v.show();
 
     return 0;
 }
